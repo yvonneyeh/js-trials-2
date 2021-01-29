c08cdfcfb3e06cf884dd2d4556c0a11d44f818cd
Yvonne Yeh <yvonneyeh@me.com>
2021-01-29 01:28:52 +0000

    Define objects.js functions

--- objects.js
+++ objects.js
@@ -3,11 +3,34 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  // Return a plain object of each word and the no. of times they appear.
+
+  const wordCounts = {}
+
+  for (const word of phrase.split(' ')) {
+    if (wordCounts[word]) {
+      wordCounts[word] += 1;
+    }
+    else {
+      wordCounts[word] = 1;
+    }
+  }
+  return wordCounts;
 }
 
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+  // Return the array of melons that cost 'price'.
+  
+  const melonPrices = {
+    '2.5': ['Canteloupe', 'Honeydew'],
+    '2.95': ['Watermelon'],
+    '3.25': ['Musk','Crenshaw'],
+    '14.25': ['Christmas']
+  };
+  if (!melonPrices[price]){
+    return null;
+  }
+  return melonPrices[price].sort();
 }
