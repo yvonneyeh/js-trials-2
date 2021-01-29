29535d561384fb9f130a386327a8add52a80ddc8
Yvonne Yeh <yvonneyeh@me.com>
2021-01-29 02:20:59 +0000

    Update functions.js

--- functions.js
+++ functions.js
@@ -3,14 +3,38 @@
 
 // 1. isHometown
 
-// Define your function here
+// Given a 'town', return 'true' if 'town' is 'San Francisco'.
+
+const isHometown = town => town === 'San Francisco';
 
 
 // 2. getFullName
 
-// Define your function here
+// Give 'firstName' and 'lastName', return a full name.
+
+const getFullName = (firstName, lastName) => `${firstName} ${lastName}`;
 
 
 // 3. calculateTotal
 
-// Define your function here
+// Return the total price of an item, figuring in state taxes and fees.
+
+const calculateTotal = (basePrice, state, tax=0.05) => {
+    const subtotal = basePrice * (1 + tax);
+    let fee = 0;
+    if (state === 'CA'){
+        fee = 0.03 * subtotal;
+    }
+    else if (state === 'PA'){
+        fee = 2;
+    }
+    else if (state === 'MA'){
+        if (basePrice <= 100){
+            fee = 1;
+        }
+        else{
+            fee = 3;
+        }
+    }
+    return subtotal + fee
+}
